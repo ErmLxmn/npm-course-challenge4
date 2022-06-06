74038113f0321d71ffb925e2c63f12f9232a4a8a
Ermel Laxamana <Ermel.Laxamana@filamsoftware.com>
2022-06-06 09:17:04 +0800

    fixed some issue in the shema

--- schema.js
+++ schema.js
@@ -1,15 +1,17 @@
 const mongoose = require('mongoose');
 
-let userSchema = new mongoose.Schema({
-    username : {type: String, require: true},
-    logs : [exerciseSchema]
-})
 
 let exerciseSchema = new mongoose.Schema({
     description: {type: String , require: true},
     duration: {type: Number , require: true},
     date: String,
 })
+
+let userSchema = new mongoose.Schema({
+    username : {type: String, require: true},
+    logs : [exerciseSchema]
+})
+
 
 exports.userSchema = userSchema;
 exports.exerciseSchema = exerciseSchema;
